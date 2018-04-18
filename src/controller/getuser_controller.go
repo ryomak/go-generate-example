6b49a9b3ee752fa507f2e9bebb5206b283a73ed4
ryomak <ryooomaaa0413@outlook.com>
2018-04-18 19:42:25 +0900

    delete file

--- src/controller/getuser_controller.go
+++
@@ -1,9 +0,0 @@
-
-package controller
-import (
-	"net/http"
-)
-
-func GetUser(w http.ResponseWriter,r *http.Request){
-	
-}
--- src/controller/notfound_controller.go
+++
@@ -1,9 +0,0 @@
-
-package controller
-import (
-	"net/http"
-)
-
-func NotFound(w http.ResponseWriter,r *http.Request){
-	
-}
